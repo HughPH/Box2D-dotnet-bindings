11f35a223d34fbc793e94f3c885f8564c0d2bcfa
Hugh Phoenix-Hulme <hugh@webnet.org.uk>
2025-05-21 07:38:54 +0100

    Restored commented publish steps Added FileNotFoundException if the library is genuinely not found

--- src/Box2DBindings/Core_Externs.cs
+++ src/Box2DBindings/Core_Externs.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace Box2D
@@ -62,6 +64,12 @@ namespace Box2D
 
         static unsafe Core()
         {
+#if DEBUG
+            if (!File.Exists(libraryName))
+            {
+                throw new FileNotFoundException($"The library {libraryName} was not found in current directory: {Environment.CurrentDirectory}");
+            }
+#endif
             var lib = NativeLibrary.Load(libraryName);
 
             NativeLibrary.TryGetExport(lib, "b2GetVersion", out var p0);
