0866d7ee8f64110025ab6c94682446fb636c6adc
Hugh Phoenix-Hulme <hugh@webnet.org.uk>
2025-06-04 10:59:00 +0100

    Updated Box2D version in VersionTest

--- src/UnitTests/Box2DVersionTest.cs
+++ src/UnitTests/Box2DVersionTest.cs
@@ -14,7 +14,7 @@ public class Box2DVersionTest
         
         var version = Box2D.Core.GetVersion();
         string versionString = $"v{version.Major}.{version.Minor}.{version.Revision}";
-        Assert.Equal("v3.1.0", versionString);
+        Assert.Equal("v3.1.1", versionString);
         
         if (error is not null) Assert.Fail(error);
     }
